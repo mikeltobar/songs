7179a048df10967d54e3b59f9c13fcde51aec530
Mikel Tobar del Barrio <81832365+mikeltobar@users.noreply.github.com>
2023-02-10 18:35:44 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="PEC4",
+    version="0.1.0",
+    url="https://github.com/",
+    author="Mikel Tobar del Barrio",
+    author_email="mtobar@uoc.edu",
+    description="Creation of a songs dataset and analysis",
+    #long_description=open('README.md').read(),
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+    ],
+    include_package_data=True,
+    package_data={'': ['data/data.zip']},
+)
